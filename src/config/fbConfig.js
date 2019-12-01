7dc3cfb18b37933ce4913b878c6e6612b561f88f
Sam Wilkinson <scw528@mail.usask.ca>
2019-12-01 16:58:21 -0600

    remove google api key

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGrRv6VsVeceBMIn4bqG7VoZz4i61gqkE",
+  apiKey: "",
   authDomain: "grubright-11f85.firebaseapp.com",
-  databaseURL: "https://grubright-11f85.firebaseio.com",
-  projectId: "grubright-11f85",
-  storageBucket: "grubright-11f85.appspot.com",
-  messagingSenderId: "594591547644",
-  appId: "1:594591547644:web:0a0665eb758ffe52"
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
